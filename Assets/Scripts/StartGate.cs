92423f5dec17e1339ab17f8668193f1d5dc50f68
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2023-03-14 23:04:28 -0400

    feat(waves): add basic wave spawner

--- Assets/Scripts/StartGate.cs
+++ Assets/Scripts/StartGate.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class StartGate : MonoBehaviour
 {
-    public Waypoint FirstWaypoint;
-    public Enemy EnemyPrefab;
+    public float timeBetweenWavesSeconds = 5.0f;
+    public float timeBetweenSpawnsSeconds = 0.1f;
+    public Waypoint firstWaypoint;
+    public Wave[] waves;
+    private int currentWaveIndex = 0;
+    private int currentSpawnCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (FirstWaypoint == null || EnemyPrefab == null) return;
+        if (firstWaypoint == null || waves == null) return;
 
-        Enemy instantiatedEnemy = Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
-        instantiatedEnemy.targetObject = FirstWaypoint.gameObject;
+        InvokeRepeating("SpawnCurrentWaveEnemy", timeBetweenWavesSeconds, timeBetweenSpawnsSeconds);
+    }
+
+    void SpawnCurrentWaveEnemy()
+    {
+        GameObject instantiated = Instantiate(waves[currentWaveIndex].enemyPrefab, transform.position, Quaternion.identity);
+        Enemy enemy = instantiated.GetComponent<Enemy>();
+        enemy.targetObject = firstWaypoint.gameObject;
+        currentSpawnCount += 1;
+
+        if (currentSpawnCount != waves[currentWaveIndex].amount) return;
+
+        currentWaveIndex += 1;
+        currentSpawnCount = 0;
+        CancelInvoke();
+
+        if (currentWaveIndex < waves.Length)
+        {
+            InvokeRepeating("SpawnCurrentWaveEnemy", timeBetweenWavesSeconds, timeBetweenSpawnsSeconds);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (FirstWaypoint == null || EnemyPrefab == null) return;
-
-
+        if (firstWaypoint == null) return;
 
     }
 }
